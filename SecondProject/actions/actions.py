2428c0dc51f53dcaa96384035317e961c5ca1045
Ben Maduabuchi <benrexocore@gmail.com>
2023-12-04 00:44:18 -0700

    added story for ordering

--- SecondProject/actions/actions.py
+++ SecondProject/actions/actions.py
@@ -20,3 +20,33 @@ class ExtractFoodEntity(Action):
             dispatcher.utter_message(text="I am sorry but I could not detect what food you want")
 
         return []
+
+
+class OrderFoodAction(Action):
+    def name(self) -> Text:
+        return "action_order_food"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        dispatcher.utter_message(text="Which kind of food would you like to get?")
+
+        return []
+
+class ConfirmOrderAction (Action):
+    def name(self) -> Text:
+        return "action_confirm_order"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        food_entity = next(tracker.get_latest_entity_values("food"), None)
+
+        if food_entity:
+            dispatcher.utter_message(text=f"Your order for the {food_entity} has been sent to be made in the kitchen")
+        else:
+            dispatcher.utter_message(text="I am sorry but I could not detect what food you want")
+
+        return []
