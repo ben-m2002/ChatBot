e864e2bde12a74a964ce8d7e9a61b4e14b615109
Ben Maduabuchi <benrexocore@gmail.com>
2023-12-18 11:13:52 -0700

    added weather API

--- WeatherBot/actions/actions.py
+++ WeatherBot/actions/actions.py
@@ -1,27 +1,43 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
-#
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/custom-actions
+from typing import Any, Text, Dict, List
+from config import API_KEY
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+import requests
 
+class ActionWeather(Action):
 
-# This is a simple example for a custom action which utters "Hello World!"
+    def name(self) -> Text:
+        return "action_get_weather"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        city = next(tracker.get_latest_entity_values("city"), None)
+        weather_data = self.get_weather(city)
+
+        print(f"city: {city}")
+        print(f"weather_data: {weather_data}")
+
+        if weather_data:
+            response = (f"The weather in {city} is {weather_data['current']['temp_f']} Fahrenheit "
+                        f"with a high of {weather_data['forecast']['forecastday'][0]['day']['maxtemp_f']}"
+                        f" and a low of {weather_data['forecast']['forecastday'][0]['day']['mintemp_f']}. ")
+        else:
+            response = f"Sorry, I couldn't find the weather for {city}"
+
+        dispatcher.utter_message(text=response)
+
+        return []
+
+    def get_weather(self, city):
+        url = f"http://api.weatherapi.com/v1/forecast.json?key={API_KEY}&q={city}&days=1&aqi=no&alerts=no"
+        try:
+            response = requests.get(url)
+            api_response = response.json()
+            print(f"api_response: {api_response}")
+            return api_response
+        except requests.exceptions.RequestException as e:
+            print(e)
+
+        return 3
